195697fdc41bd8dfa92013a558f67e4037c61411
helmitaqiyudin <helmitaqiyudin@gmail.com>
2024-11-15 16:37:54 +0700

    feat: implement tabs for timeline section

--- src/components/custom/timelineContainer.tsx
+++ src/components/custom/timelineContainer.tsx
@@ -1,44 +1,26 @@
 "use client"
 
-import { useState } from "react"
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import EducationTimeline from "./eduTimeline"
 import ExperienceTimeline from "./expTimeline"
 
 export default function TimelineContainer() {
-    const [activeTab, setActiveTab] = useState<"experience" | "education">("experience")
-
     return (
         <div className="w-full max-w-[650px] mx-auto">
             <Card className="bg-transparent">
-                <div className="grid grid-cols-2 p-1 gap-1">
-                    <Button
-                        variant="ghost"
-                        className={cn(
-                            "rounded-lg",
-                            activeTab === "experience" && "bg-gray-200 dark:bg-gray-800"
-                        )}
-                        onClick={() => setActiveTab("experience")}
-                    >
-                        Experience
-                    </Button>
-                    <Button
-                        variant="ghost"
-                        className={cn(
-                            "rounded-lg",
-                            activeTab === "education" && "bg-gray-200 dark:bg-gray-800"
-                        )}
-                        onClick={() => setActiveTab("education")}
-                    >
-                        Education
-                    </Button>
-                </div>
-                <div className="p-4">
-                    {activeTab === "experience" && <ExperienceTimeline />}
-                    {activeTab === "education" && <EducationTimeline />}
-                </div>
+                <Tabs defaultValue="experience" className="w-full">
+                    <TabsList className="rounded-b-none grid w-full grid-cols-2">
+                        <TabsTrigger value="experience">Experience</TabsTrigger>
+                        <TabsTrigger value="education">Education</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="experience" className="p-4">
+                        <ExperienceTimeline />
+                    </TabsContent>
+                    <TabsContent value="education" className="p-4">
+                        <EducationTimeline />
+                    </TabsContent>
+                </Tabs>
             </Card>
         </div>
     )
